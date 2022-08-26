cee5b53f79ed68d6f3a52ad2e25a709ab10e2106
Maurits van der Schee <maurits@vdschee.nl>
2022-08-26 03:39:46 +0200

    update

--- GenerateViews.cs
+++ GenerateViews.cs
@@ -1,7 +1,4 @@
-﻿// SayHello.cs
-using Microsoft.Build.Framework;
-using System;
-using System.IO;
+﻿using System.IO;
 using System.Xml;
 
 namespace Maussoft.Mvc.ViewGen
@@ -10,72 +7,66 @@ namespace Maussoft.Mvc.ViewGen
     {
         public override bool Execute()
         {
-            string directory = Directory.GetCurrentDirectory ();
-			string viewDirectory = Path.Combine(directory,"Views");
-			string viewNamespace = FindViewNamespaceInAppConfig (directory, "App.config");
-			string rootNamespace = FindRootNamespaceInProjectFile (directory, "*.csproj");
-			string language = "C#";
-			if (rootNamespace == null) {
-				rootNamespace = FindRootNamespaceInProjectFile (directory, "*.vbproj");
-				language = "VB";
-			}
-			if (rootNamespace == null) {
-				Log.LogMessage("Could not find project file (*.csproj or *.vbproj)");
+            string directory = Directory.GetCurrentDirectory();
+            string viewDirectory = Path.Combine(directory, "Views");
+            string viewNamespace = null;
+            string rootNamespace = FindRootNamespaceInProjectFile(directory, "*.csproj");
+            string language = "C#";
+            if (rootNamespace == null)
+            {
+                rootNamespace = FindRootNamespaceInProjectFile(directory, "*.vbproj");
+                language = "VB";
+            }
+            if (rootNamespace == null)
+            {
+                Log.LogMessage("Could not find project file (*.csproj or *.vbproj)");
                 return false;
-			}
-			if (viewNamespace==null) {
-				viewNamespace = rootNamespace + '.' + "Views";
-			}
-			string[] files = Directory.GetFiles(viewDirectory, "*.aspx", SearchOption.AllDirectories);
+            }
+            if (viewNamespace == null)
+            {
+                viewNamespace = rootNamespace + '.' + "Views";
+            }
+            string[] files = Directory.GetFiles(viewDirectory, "*.aspx", SearchOption.AllDirectories);
 
-			Generator generator; 
-			if (language == "C#") {
-				generator = new CSharpGenerator (viewDirectory, viewNamespace);
-			} else {
-				generator = new VisualBasicGenerator (viewDirectory, viewNamespace, rootNamespace);
-			}
+            Generator generator;
+            if (language == "C#")
+            {
+                generator = new CSharpGenerator(viewDirectory, viewNamespace);
+            }
+            else
+            {
+                generator = new VisualBasicGenerator(viewDirectory, viewNamespace, rootNamespace);
+            }
 
-			foreach (string filename in files) {
-				Log.LogMessage ('.'+filename.Substring(directory.Length));
-				generator.ConvertFile (filename);
-			}
+            foreach (string filename in files)
+            {
+                Log.LogMessage('.' + filename.Substring(directory.Length));
+                generator.ConvertFile(filename);
+            }
             return true;
-		}
+        }
 
-		static string FindViewNamespaceInAppConfig(string directory, string filename)
-		{
-			string fullPath = Path.Combine (directory, filename);
-			if (File.Exists (fullPath)) {
-				XmlDocument xmlDoc = new XmlDocument();
-				xmlDoc.Load(fullPath);
-				XmlNamespaceManager nsManager = new XmlNamespaceManager(xmlDoc.NameTable);
-				nsManager.AddNamespace("c", xmlDoc.DocumentElement.NamespaceURI);
-				XmlNode node = xmlDoc.SelectSingleNode("/c:configuration/c:appSettings/c:add[@key='Maussoft.Mvc.ViewNamespaces']/@value",nsManager);
-				if (node!=null) {
-					return (node as XmlAttribute).Value.Split(',')[0];
-				}
-			}
-			return null;
-		}
-
-		static string FindRootNamespaceInProjectFile(string directory, string glob)
-		{
-			string[] files = Directory.GetFiles(directory, glob, SearchOption.TopDirectoryOnly);
-			foreach (string fullPath in files) {
-				if (File.Exists (fullPath)) {
-					XmlDocument xmlDoc = new XmlDocument();
-					xmlDoc.Load(fullPath);
-					XmlNamespaceManager nsManager = new XmlNamespaceManager(xmlDoc.NameTable);
-					nsManager.AddNamespace("p", xmlDoc.DocumentElement.NamespaceURI);
-					XmlNode node = xmlDoc.SelectSingleNode("/p:Project/p:PropertyGroup/p:RootNamespace",nsManager);
-					if (node != null) {
-						return node.InnerText;
-					}
-				}
-			}
-			return null;
-		}
+        private static string FindRootNamespaceInProjectFile(string directory, string glob)
+        {
+            string[] files = Directory.GetFiles(directory, glob, SearchOption.TopDirectoryOnly);
+            foreach (string fullPath in files)
+            {
+                if (File.Exists(fullPath))
+                {
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(fullPath);
+                    XmlNamespaceManager nsManager = new XmlNamespaceManager(xmlDoc.NameTable);
+                    nsManager.AddNamespace("p", xmlDoc.DocumentElement.NamespaceURI);
+                    XmlNode node = xmlDoc.SelectSingleNode("/p:Project/p:PropertyGroup/p:RootNamespace", nsManager);
+                    if (node != null)
+                    {
+                        return node.InnerText;
+                    }
+                }
+            }
+            return null;
+        }
 
 
-	}
+    }
 }
--- Generator.cs
+++ Generator.cs
@@ -1,94 +1,107 @@
 ﻿using System;
-using System.IO;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 
 namespace Maussoft.Mvc.ViewGen
 {
-	public class Token
-	{
-		public Token(string type,string value)
-		{ 
-			Type = type;
-			Value = value;
-		}
+    public class Token
+    {
+        public Token(string type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
 
-		public string Type;
-		public string Value;
-	}
+        public string Type;
+        public string Value;
+    }
 
-	public class Statement
-	{
-		public Statement(string type, Token[] tokens)
-		{ 
-			Type = type;
-			Tokens = tokens;
-		}
+    public class Statement
+    {
+        public Statement(string type, Token[] tokens)
+        {
+            Type = type;
+            Tokens = tokens;
+        }
 
-		public string Type;
-		public Token[] Tokens;
-	}
+        public string Type;
+        public Token[] Tokens;
+    }
 
-	public class Generator
-	{
-		public virtual void ConvertFile (string filename)
-		{
+    public class Generator
+    {
+        public virtual void ConvertFile(string filename)
+        {
 
-		}
+        }
 
-		protected void AddTokenListAsStatement(List<Statement> statements, List<Token> statement)
-		{
-			if (statement.Count > 0) {
-				statements.Add (new Statement(statement[0].Type, statement.ToArray()));
-			}
-		}
+        protected void AddTokenListAsStatement(List<Statement> statements, List<Token> statement)
+        {
+            if (statement.Count > 0)
+            {
+                statements.Add(new Statement(statement[0].Type, statement.ToArray()));
+            }
+        }
 
-		protected void ParsePropertiesInHead(Dictionary <string,string> properties, Token head)
-		{
-			// simple parser
-			string regex = @"([a-zA-Z]+)(\s*=\s*""([^""]+?)"")?";
-			foreach (Match match in Regex.Matches (head.Value, regex, RegexOptions.IgnoreCase)) {
-				string key = match.Groups [1].Value;
-				if ( match.Groups [2].Value.Length>0) {
-					string value = match.Groups [3].Value;
-					properties [key] = value;
-				} else {
-					properties ["Type"] = key;
-				}
-			}
-		}
+        protected void ParsePropertiesInHead(Dictionary<string, string> properties, Token head)
+        {
+            // simple parser
+            string regex = @"([a-zA-Z]+)(\s*=\s*""([^""]+?)"")?";
+            foreach (Match match in Regex.Matches(head.Value, regex, RegexOptions.IgnoreCase))
+            {
+                string key = match.Groups[1].Value;
+                if (match.Groups[2].Value.Length > 0)
+                {
+                    string value = match.Groups[3].Value;
+                    properties[key] = value;
+                }
+                else
+                {
+                    properties["Type"] = key;
+                }
+            }
+        }
 
-		protected List<Statement> Parse(List<Token> input, Dictionary <string,string> properties)
-		{
-			List<Statement> statements = new List<Statement> ();
-			List<Token> statement = new List<Token>();
+        protected List<Statement> Parse(List<Token> input, Dictionary<string, string> properties)
+        {
+            List<Statement> statements = new List<Statement>();
+            List<Token> statement = new List<Token>();
 
-			for (int i=0;i<input.Count;i++) {
-				if (input [i].Type == "Head") {
-					AddTokenListAsStatement (statements, statement);
-					ParsePropertiesInHead(properties, input [i]);
-					statement = new List<Token>();
-				} else if (input [i].Type == "Code") {
-					AddTokenListAsStatement (statements, statement);
-					statements.Add (new Statement("Code",new Token[]{input [i]}));
-					statement = new List<Token>();
-				} else if (input [i].Type == "NewLine") {
-					if (statement.Count > 0) {
-						if (statement [0].Type == "Text") {
-							statement [0].Type = "TextLine";
-						}
-					}
-					AddTokenListAsStatement (statements, statement);
-					statements.Add (new Statement("NewLine",new Token[]{input [i]}));
-					statement = new List<Token>();
-				} else {
-					statement.Add (input [i]);
-				}
-			}
-			AddTokenListAsStatement (statements, statement);
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i].Type == "Head")
+                {
+                    AddTokenListAsStatement(statements, statement);
+                    ParsePropertiesInHead(properties, input[i]);
+                    statement = new List<Token>();
+                }
+                else if (input[i].Type == "Code")
+                {
+                    AddTokenListAsStatement(statements, statement);
+                    statements.Add(new Statement("Code", new Token[] { input[i] }));
+                    statement = new List<Token>();
+                }
+                else if (input[i].Type == "NewLine")
+                {
+                    if (statement.Count > 0)
+                    {
+                        if (statement[0].Type == "Text")
+                        {
+                            statement[0].Type = "TextLine";
+                        }
+                    }
+                    AddTokenListAsStatement(statements, statement);
+                    statements.Add(new Statement("NewLine", new Token[] { input[i] }));
+                    statement = new List<Token>();
+                }
+                else
+                {
+                    statement.Add(input[i]);
+                }
+            }
+            AddTokenListAsStatement(statements, statement);
 
-			/*for (int i = 0; i < statements.Count; i++) {
+            /*for (int i = 0; i < statements.Count; i++) {
 				Token[] tokens = statements[i].Tokens;
 				for (int j = 0; j < tokens.Length; j++) {
 					Token token = tokens [j];
@@ -97,43 +110,51 @@ namespace Maussoft.Mvc.ViewGen
 				Console.WriteLine ();
 			}*/
 
-			return statements;
-		}
+            return statements;
+        }
 
 
-		protected void AddTokens(List<Token> tokens, string type, string value)
-		{
-			string[] lines = value.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
-			for (int i=0;i<lines.Length;i++) {
-				tokens.Add (new Token (type, lines [i]));
-				if (i < lines.Length - 1) {
-					tokens.Add (new Token ("NewLine", ""));
-				}
-			}
-		}
+        protected void AddTokens(List<Token> tokens, string type, string value)
+        {
+            string[] lines = value.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                tokens.Add(new Token(type, lines[i]));
+                if (i < lines.Length - 1)
+                {
+                    tokens.Add(new Token("NewLine", ""));
+                }
+            }
+        }
 
-		protected List<Token> Tokenize(string input)
-		{
-			int start,end = 0;
-			List<Token> tokens = new List<Token>();
+        protected List<Token> Tokenize(string input)
+        {
+            int start, end = 0;
+            List<Token> tokens = new List<Token>();
 
-			while ((start = input.IndexOf("<%",end))>=0) {
-				AddTokens(tokens,"Text",input.Substring (end, start - end));
-				end = input.IndexOf("%>",start);
-				if (end<0) break;
-				if (input[start+2]=='=') {
-					AddTokens(tokens,"Expr",input.Substring (start+3,end-(start+3)));
-				} else if (input[start+2]=='@') {
-					AddTokens(tokens,"Head",input.Substring (start+3,end-(start+3)));
-				} else {
-					AddTokens(tokens,"Code",input.Substring (start+3,end-(start+3)));
-				}
-				end = end + 2;
-			}
-			AddTokens(tokens,"Text",input.Substring (end));
+            while ((start = input.IndexOf("<%", end)) >= 0)
+            {
+                AddTokens(tokens, "Text", input.Substring(end, start - end));
+                end = input.IndexOf("%>", start);
+                if (end < 0) break;
+                if (input[start + 2] == '=')
+                {
+                    AddTokens(tokens, "Expr", input.Substring(start + 3, end - (start + 3)));
+                }
+                else if (input[start + 2] == '@')
+                {
+                    AddTokens(tokens, "Head", input.Substring(start + 3, end - (start + 3)));
+                }
+                else
+                {
+                    AddTokens(tokens, "Code", input.Substring(start + 3, end - (start + 3)));
+                }
+                end = end + 2;
+            }
+            AddTokens(tokens, "Text", input.Substring(end));
 
-			return tokens;
-		}
-	}
+            return tokens;
+        }
+    }
 }
 
